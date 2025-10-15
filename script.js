46972c6bf64a981770dd80248b77e43948a5ebf4
Yasmim Freitas <yasmimfreitas0502@gmail.com>
2025-10-15 18:07:23 -0300

    Deixando meu menu responsivo

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const linksInternos = document.querySelectorAll('.nav_links a[href^="#"]');
+// Obtém o ícone de menu e a lista de links
+const menuIcon = document.querySelector('.menu-icon');
+const menuList = document.querySelector('.menu-list');
 
-    function scrollToSection(event) {
+// Função para alternar a visibilidade do menu
+function toggleMenu() {
+    menuList.classList.toggle('is-open');
+}
+
+// Adiciona um evento de clique no ícone de menu
+menuIcon.addEventListener('click', (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    toggleMenu();
+});
+
+// Obtém todos os links internos do menu
+const linksInternos = document.querySelectorAll('.menu-list a[href^="#"]');
+
+// Adiciona um evento de clique para cada link do menu
+linksInternos.forEach(link => {
+    link.addEventListener('click', (event) => {
+        // Rola a página para a seção
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
@@ -8,11 +27,11 @@ const linksInternos = document.querySelectorAll('.nav_links a[href^="#"]');
             behavior: 'smooth',
             block: 'start',
         });
-    }
-
-    linksInternos.forEach(link => {
-        link.addEventListener('click', scrollToSection);
+        
+        // Esconde o menu depois de clicar em um link
+        menuList.classList.remove('is-open');
     });
+});
     
 
 
